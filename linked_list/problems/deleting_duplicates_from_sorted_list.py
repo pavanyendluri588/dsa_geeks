65fbc5a47468eec1aa68a7a4e7d599c4dfdc8e68
pavanyendluri588 <pavanyendluri588@gmail.com>
2023-01-06 15:11:08 +0530

    prathice and improve

--- linked_list/problems/deleting_duplicates_from_sorted_list.py
+++ linked_list/problems/deleting_duplicates_from_sorted_list.py
@@ -20,9 +20,26 @@ class linked_list(Node):
         while new_address!=None:
             print(new_address.data)
             new_address=new_address.next
+    def delete_duplicate(self):
+        self.new_address1=self.head
+        data1=self.new_address1.data
+        while self.new_address1!=None:
+            data1=self.new_address1.data
+            if self.new_address1.next!=None:
+                data1=self.new_address1.data
+                if self.new_address1.next.data==data1:
+                    self.new_address1.next=self.new_address1.next.next
+            self.new_address1=self.new_address1.next
+            
+
+
 obj=linked_list()
+obj.insert(1)
 obj.insert(1)
 obj.insert(2)
 obj.insert(2)
 obj.insert(3)
+obj.insert(3)
+obj.access()
+obj.delete_duplicate()
 obj.access()
